8472880fc443a2e1b0b25405848e7907eb19aa2e
Daniel M <danielmiao2019@icloud.com>
2021-10-06 10:38:08 -0700

    updated allowed_hosts

--- ChessWebsite/settings.py
+++ ChessWebsite/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["KEY"]
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
